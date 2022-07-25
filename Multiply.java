8b173fdcfc606c52b91e925b876b03b1aeb42b08
Abhijeet Singh <abhijeet.ch.786@gmail.com>
2022-07-25 22:11:45 +0530

    Adding multiply functionalityh

--- Multiply.java
+++ Multiply.java
@@ -1,7 +1,7 @@
 public class Multiply {
     public static void main(String[] args) {
-        System.out.println("hello")
-        
-        ;
+      int a=4;
+      int b=5;
+      System.out.println("a*b");
     }
 }
