9e731e31ea49aaa3de5ff6c5469284626faa935f
Abhijeet Singh <abhijeet.ch.786@gmail.com>
2022-07-25 22:31:06 +0530

    LabResultController Feature Update

---
+++ LabResultController.java
@@ -0,0 +1,61 @@
+public class Multiply {
+    public static void main(String[] args) {
+      int a=4;
+      int b=5;
+      System.out.println("a*b");
+      dkdfihdf\
+      dfhkdfhdkfj
+      dhfdihfiherif
+      drhfdghisfhsef
+      edfishefishef
+      ehiewhfihseri
+      erhedshfiksdjfikjdf
+      ehiehriejhr
+      erjhierhiehr
+      erhjehrierhire
+      erhierjhierher
+      erhierjhierher]erhjehrierhire\erjhierhiehr
+
+      erhedshfiksdjfikjdfr
+      erhedshfiksdjfikjdfer
+      erhedshfiksdjfikjdferer
+      erhedshfiksdjfikjdf
+      erhedshfiksdjfikjdferer
+      erhedshfiksdjfikjdfer
+      public static gh
+      hg
+      try (er
+      er
+      
+      erhedshfiksdjfikjdfrt
+      rt
+      rt
+      ret
+      retf
+      staticdf
+      fgd
+      tryt
+      
+      retth
+      gfsd
+      
+      rfety) {dfhkdfhdkfjdf
+        
+      } catch (Exception e) {
+        //TODO: handle exception
+      } name() {
+        dfhkdfhdkfjdfdfkdf
+        dfhkdfhdkfjdfdfkdfdf
+        dfhkdfhdkfjdf
+        dfhkdfhdkfjdfdfkdfdfdfcv
+        df
+
+        drhfdghisfhseff
+        df
+
+        dfhkdfhdkfjdgf
+        dfhkdfhdkfjdf
+        
+      }
+    }
+}
